5e2ce0e46064f49d4ab24299566c5bea6be4678a
administrator <administrator@1887.local>
2020-01-02 07:26:12 -0800

    Fixing exporting function

--- src/exports.js
+++ src/exports.js
@@ -1,6 +1,6 @@
 import sketch from 'sketch'
 // documentation: https://developer.sketchapp.com/reference/api/
 
-function exportXaml(context){
+export function exportXaml(context){
   sketch.UI.alert("Am I alive?", "It's alive 🙌")
 }
